129cd8d08c3a0a0a1b97fcf62056eb70f2281645
admin <admin@Bcalab4-m06>
2022-05-30 10:48:58 +0530

    commit 1

---
+++ journal/src/journal/Orange.java
@@ -0,0 +1,24 @@
+//roll no: 124
+package journal;
+
+public class Orange {
+	String color;
+	Orange(String color)
+	{
+		this.color=color;
+	}
+	String getColor()
+	{
+		return color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Orange greenApple = new Orange("green");
+		String color = greenApple.getColor();
+		System.out.println("color of apple is "+ color);
+
+	}
+
+}
